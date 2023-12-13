cb015dabd454ad46de780a2dd924b87cf32eb0cb
junemo <junemo010107@gmail.com>
2023-12-14 01:03:52 +0900

    move=gif complete

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+//define variable
+const moveItemInners=document.querySelectorAll(".move-item-inner");
 //making youtube video
 const tag = document.createElement('script');
-
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -45,3 +46,28 @@ function calculatePlayerHeight() {
   // 높이를 60%로 계산하여 반환
   return screenHeight * 0.8;
 }
+
+//move-pic hover event(convert to gif)
+moveItemInners.forEach(function(moveItemInner) {
+  moveItemInner.addEventListener("mouseenter", function() {
+    const movePic = moveItemInner.querySelector(".move-pic");
+    const moveGif = moveItemInner.querySelector(".move-gif");
+    movePic.style.display="none";
+    moveGif.style.display="block";
+    moveGif.style.filter = "grayscale(0)";
+    moveGif.style.transform = "scale(1.1)";
+    
+  });
+
+  moveItemInner.addEventListener("mouseleave", function() {
+    const movePic = moveItemInner.querySelector(".move-pic");
+    const moveGif = moveItemInner.querySelector(".move-gif");
+
+    movePic.style.filter = "";
+    movePic.style.transform = "";
+    moveGif.style.display = "none"; // 이미지 변경을 위한 코드 (GIF 이미지로 교체하는 로직을 추가해야 함)
+    movePic.style.display="block";
+  });
+});
+
+
